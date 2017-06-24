7a0451d861e49665647685a33ca14405a0e0036a
ignitz <ignitzhjfk@gmail.com>
2017-06-24 14:59:11 -0300

    Removing hardcoded URLs in templates

--- src/backend/lunnarfallinn/polls/urls.py
+++ src/backend/lunnarfallinn/polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.detail, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.detail, name='vote'),
 ]
