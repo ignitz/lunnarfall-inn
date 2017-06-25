e46a7cc8f01455a7e6f25584efd16ada0547546f
ignitz <ignitzhjfk@gmail.com>
2017-06-24 21:41:02 -0300

    Antes de mudar endereço

--- src/lunnarfall/peoples/admin.py
+++ src/lunnarfall/peoples/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Pessoa
 
-# Register your models here.
+admin.site.register(Pessoa)
+
+# admin
+# 1q2w3e4r5t
--- src/lunnarfall/rooms/apps.py
+++ src/lunnarfall/rooms/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class RoomsConfig(AppConfig):
     name = 'rooms'
